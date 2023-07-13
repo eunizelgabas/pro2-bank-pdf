767407e025b5af86140bf4c16975f51db954c5f8
eunizelgabas <eunizelgabas24@gmail.com>
2023-07-13 11:40:55 +0800

    mailing

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -13,6 +13,52 @@ class ClientSeeder extends Seeder
      */
     public function run(): void
     {
-        Client::factory(30)->create();
+        Client::factory(20)->create();
+        $cl = [
+            [
+                'last_name' => 'Lenteria',
+                'first_name' => 'Benjie',
+                'middle_name' => 'Basio',
+                'address' => 'Pob. Centro, Clarin, Bohol',
+                'birth_date' => '1979-10-02',
+                'phone_number' => '09090892350',
+                'email' => 'benjielenteria@yahoo.com',
+                'initial_deposit' => 1000,
+            ],
+            [
+                'last_name' => 'Lentrix',
+                'first_name' => 'Hawkman',
+                'middle_name' => 'Machiacode',
+                'address' => 'Pob. Centro, Clarin, Bohol',
+                'birth_date' => '1979-10-02',
+                'phone_number' => '09090892350',
+                'email' => 'hawkmanlentrix@gmail.com',
+                'initial_deposit' => 1000,
+            ],
+            [
+                'last_name' => 'Iphitech',
+                'first_name' => 'Bench',
+                'middle_name' => 'Developer',
+                'address' => 'Pob. Centro, Clarin, Bohol',
+                'birth_date' => '1979-10-02',
+                'phone_number' => '09090892350',
+                'email' => 'iphitech.lenteria.benjie@gmail.com',
+                'initial_deposit' => 1000,
+            ],
+            [
+                'last_name' => 'Gabas',
+                'first_name' => 'Eunizel',
+                'middle_name' => 'Requierme',
+                'address' => 'Santa Cruz, Sagbayan, Bohol',
+                'birth_date' => '2002-06-24',
+                'phone_number' => '09518994185',
+                'email' => 'eunizelgabas06@gmail.com',
+                'initial_deposit' => 1000,
+            ],
+        ];
+
+        foreach($cl as $c) {
+            Client::create($c);
+        }
     }
 }
